2cea8c9fb2cb66143dfc4cd87bbf4a8bfc72e770
Rahul R <157510957+RAHUL311RANGASAMY@users.noreply.github.com>
2025-12-31 00:30:54 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,8 @@
-// const app = require("./app");
+const app = require("./app");
 
-// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
-const express = require("express");
-const cors = require("cors");
-
-const app = express();
-
-// Middleware
-app.use(cors());              // ✅ ALLOW ALL ORIGINS
-app.use(express.json());
-
-// Routes
-app.use("/api/paste", require("./routes/pasteRoutes"));
-
-module.exports = app;
