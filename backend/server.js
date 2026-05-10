2d89d5f3936a973f3455cb798131636bee6f01bd
Rahul Rangasamy <157510957+RAHUL311RANGASAMY@users.noreply.github.com>
2026-05-10 13:17:29 +0530

    Set up Express server with MongoDB and CORS

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,33 @@
-const app = require("./app");
 
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  methods: ["GET", "POST"],
+}));
+
+
+// Middleware
+app.use(express.json());
+
+
+
+// Routes
+app.use('/api/paste', require('./routes/PasteRoutes'));
+// MongoDB connect
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error(err));
+
+// Use Render's PORT
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
